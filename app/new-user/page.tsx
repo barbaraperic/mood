2e68bce043fe007e763fe3b05e932545ea2f44c6
Barbara Peric <b.peric10@gmail.com>
2024-01-09 16:25:43 +0000

    entry cards

--- app/new-user/page.tsx
+++ app/new-user/page.tsx
@@ -1,5 +1,30 @@
-const NewUserPage = () => {
-  return <div>Hi</div>
+import { prisma } from '@/utils/db'
+import { currentUser } from '@clerk/nextjs'
+import { redirect } from 'next/navigation'
+
+const createNewUser = async () => {
+  const user = await currentUser()
+  const match = await prisma.user.findUnique({
+    where: {
+      clerkId: user?.id as string,
+    },
+  })
+
+  if (!match) {
+    await prisma.user.create({
+      data: {
+        clerkId: user?.id as string,
+        email: user?.emailAddresses[0].emailAddress as string,
+      },
+    })
+  }
+
+  redirect('/journal')
+}
+
+const NewUserPage = async () => {
+  await createNewUser()
+  return <div>...loading</div>
 }
 
 export default NewUserPage
--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,18 @@
 import Link from 'next/link'
+import { auth } from '@clerk/nextjs'
 
-export default function Home() {
+export default async function Home() {
+  const { userId } = await auth()
+
+  let href = userId ? '/journal' : '/new-user'
+
   return (
     <div className="w-screen h-screen bg-black flex justify-center items-center text-white">
       <div className="mx-auto space-y-5 w-full max-w-[600px]">
         <h1 className="text-4xl">The best journal App, period.</h1>
         <p>This is the best app for tracking your mood thoughout </p>
         <div>
-          <Link href="/journal">
+          <Link href={href}>
             <button className="bg-blue-700 p-4 rounded-lg text-xl">
               get started
             </button>
